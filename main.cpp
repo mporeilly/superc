4df062e67a9cacf0d2531fe53e6b10c0ea3329cc
mac <mac@local>
2023-12-13 17:17:02 -0700

    Works with c++

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main(void)
     {
         BeginDrawing();
             ClearBackground(RAYWHITE);
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+            DrawText("Congrats! You created your first window finally have it running on this mac linux abomination!", 190, 200, 20, LIGHTGRAY);
         EndDrawing();
     }
 
